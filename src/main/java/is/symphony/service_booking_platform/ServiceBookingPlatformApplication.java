5af3ed912258fb6c087fe7359cf5b4afef78d18a
Anja Ljiljak <anjaljiljak454@gmail.com>
2025-08-05 10:52:36 +0200

    initial commit
    pocetni spring projekat

---
+++ src/main/java/is/symphony/service_booking_platform/ServiceBookingPlatformApplication.java
@@ -0,0 +1,13 @@
+package is.symphony.service_booking_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBookingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBookingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/is/symphony/service_booking_platform/ServiceBookingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package is.symphony.service_booking_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBookingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
