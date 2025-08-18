66f405c8458b832eb3a633f33c0c0e5f873318a9
Slaviša Čovakušić <slavisacovakusic@gmail.com>
2025-08-18 13:18:55 +0200

    automatske izmjene
    Co-Authored-By: Anja Ljiljak <153135831+AnjaLj@users.noreply.github.com>

--- backend/src/main/java/is/symphony/service_booking_platform/repository/BookingRepository.java
+++ backend/src/main/java/is/symphony/service_booking_platform/repository/BookingRepository.java
@@ -13,25 +13,28 @@ import is.symphony.service_booking_platform.model.Booking;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
-    @Query("SELECT b FROM Booking b " +
-            "JOIN FETCH b.availability a " +
-            "JOIN FETCH a.template t " +
-            "WHERE a.date = :targetDate")
-    List<Booking> findBookingsByDate(@Param("targetDate") LocalDate targetDate);
+        @Query("SELECT b FROM Booking b " +
+                        "JOIN FETCH b.availability a " +
+                        "JOIN FETCH a.template t " +
+                        "WHERE a.date = :targetDate")
+        List<Booking> findBookingsByDate(@Param("targetDate") LocalDate targetDate);
 
-    Optional<Booking> findByAvailabilityId(Long availabilityId);
+        Optional<Booking> findByAvailabilityId(Long availabilityId);
 
-    List<Booking> findByClientIdOrderByAvailabilityDateDescAvailabilityTemplateStartTimeDesc(Long clientId);
+        List<Booking> findByClientIdOrderByAvailabilityDateDescAvailabilityTemplateStartTimeDesc(Long clientId);
 
-    List<Booking> findByClientId(Long clientId);
+        List<Booking> findByClientId(Long clientId);
 
-    void deleteByAvailability_ServiceId(Long serviceId);
+        void deleteByAvailability_ServiceId(Long serviceId);
 
-    @Query("SELECT b FROM Booking b " +
-            "JOIN b.availability a " +
-            "WHERE a.isBooked = true AND b.reminderSent = false AND " +
-            "FUNCTION('TIMESTAMP', a.date, a.template.startTime) BETWEEN :from AND :to")
-    List<Booking> findUpcomingBookingsForReminder(
-            @Param("from") LocalDateTime from,
-            @Param("to") LocalDateTime to);
+        @Query("SELECT b FROM Booking b " +
+                        "JOIN b.availability a " +
+                        "WHERE a.isBooked = true AND b.reminderSent = false AND " +
+                        "FUNCTION('TIMESTAMP', a.date, a.template.startTime) BETWEEN :from AND :to")
+        List<Booking> findUpcomingBookingsForReminder(
+                        @Param("from") LocalDateTime from,
+                        @Param("to") LocalDateTime to);
+
+        void deleteByClientId(Long clientId);
+
 }
