3e33dc022dff26a7093c57a9ce25c83dd455d2f3
Slaviša Čovakušić <slavisacovakusic@gmail.com>
2025-08-22 15:08:12 +0200

    Obrisani sabloni za vrijeme zbog dodavanja varijabilnih vremena na osnovu duzine trajanja servisa

--- backend/src/main/java/is/symphony/service_booking_platform/repository/TimeTemplateRepository.java
+++
@@ -1,7 +0,0 @@
-package is.symphony.service_booking_platform.repository;
-
-import is.symphony.service_booking_platform.model.TimeTemplate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TimeTemplateRepository extends JpaRepository<TimeTemplate, Long> {
-}
